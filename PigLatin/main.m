3f2d4cfe0516db2d1f429657af48651d16e034d9
Vinny Tiwari <tiwari.vineet@outlook.com>
2015-05-10 11:26:05 -0700

    Initial Commit

---
+++ PigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatin
+//
+//  Created by Vineet Tiwari on 2015-05-10.
+//  Copyright (c) 2015 vinny.co. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
